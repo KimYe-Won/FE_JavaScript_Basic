bfd82fc782d02952be95c8ac4d5719e57c3ca079
kyw <kyw010103@gmail.com>
2023-03-31 20:46:05 +0900

    완료

--- task2.js
+++ task2.js
@@ -1,1 +1,15 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    var answer = [];
+    let even=0,odd=0;
+    
+    for(let i=0;i<num_list.length;i++)
+        {
+            if (num_list[i]%2===0) even+=1;
+                else odd+=1;
+        }
+    
+    answer.push(even);
+    answer.push(odd);
+    return answer;
+}
--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+    var answer = 1;
+    var mul=1;
+    while(answer<=n) {
+        answer*=(mul);
+        mul+=1;
+    
+    }
+    return mul-2;
+}
